c9f9fda7ff9b5ba384210e98f4e7da152a401d8c
Joseph Bonilla <bonillajeb@gmail.com>
2020-04-17 14:44:50 +0800

    switch to spf13-vim

--- vim/install.sh
+++ vim/install.sh
@@ -2,12 +2,14 @@
 
 dir="$(cd "$(dirname "${script_name}")" && pwd)"
 
-for vimrc in .vimrc.local .vimrc.local.plugins .vimrc.local.before
+for vimrc in .vimrc.local .vimrc.local .vimrc.bundles.local
 do
   ln -s "${dir}"/"${vimrc}" ~/"${vimrc}"
 done
 
 # install https://github.com/luan/vimfiles
-curl vimfiles.luan.sh/install | bash
+sh <(curl https://j.mp/spf13-vim3 -L)
+ln -s "${dir}"/init.vim "${HOME}"/.vim/init.vim
+ln -s "${HOME}"/.vim "${HOME}"/.config/nvim
 
 ln -s "${dir}"/colors "${HOME}"/.vim/colors
